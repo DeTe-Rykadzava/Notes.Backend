fbabb72fc2f5267ea79d65deab579469780437bd
DeTe-Rykadzava <di3378780@gmail.com>
2023-06-14 15:59:59 +0400

    Initial commit

---
+++ Core/Notes.Application/Notes/Commands/UpdateNote/UpdateNoteCommandHandler.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Notes.Application.Common.Exceptions;
+using Notes.Application.Interfaces;
+using Notes.Domain;
+
+namespace Notes.Application.Notes.Commands.UpdateNote;
+
+public class UpdateNoteCommandHandler 
+    : IRequestHandler<UpdateNoteCommand>
+{
+    private readonly INotesDbContext _dbContext;
+
+    public UpdateNoteCommandHandler(INotesDbContext dbContext) =>
+        _dbContext = dbContext;
+    
+    public async Task Handle(UpdateNoteCommand request, CancellationToken cancellationToken)
+    {
+        // TODO: исправить, если не будет работать на Task<Unit>
+        var entity = await _dbContext.Notes.FirstOrDefaultAsync(x => x.Id == request.Id, cancellationToken);
+
+        if (entity == null || entity.UserId != request.UserId)
+        {
+            throw new NotFoundException(nameof(Note),request.Id);
+        }
+
+        entity.Details = request.Details;
+        entity.Title = request.Title;
+        entity.EditDate = DateTime.Now;
+
+        await _dbContext.SaveChangesAsync(cancellationToken);
+    }
+}
---
+++ Core/Notes.Application/Notes/Queries/GetNoteDetails/GetNoteDetailsHandler.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Notes.Application.Common.Exceptions;
+using Notes.Application.Interfaces;
+using Notes.Domain;
+
+namespace Notes.Application.Notes.Queries.GetNoteDetails;
+
+public class GetNoteDetailsHandler
+    :IRequestHandler<GetNoteDetailsQuery, NoteDetailsVm>
+{
+    private readonly INotesDbContext _dbContext;
+    private readonly IMapper _mapper;
+
+    public GetNoteDetailsHandler(INotesDbContext dbContext, IMapper mapper) =>
+        (_dbContext, mapper) = (dbContext, mapper);
+    
+    public async Task<NoteDetailsVm> Handle(GetNoteDetailsQuery request, CancellationToken cancellationToken)
+    {
+        var entity = await _dbContext.Notes.FirstOrDefaultAsync(
+            note => note.Id == request.Id, cancellationToken);
+
+        if (entity == null || entity.UserId != request.UserId)
+        {
+            throw new NotFoundException(nameof(Note), request.Id);
+        }
+
+        return _mapper.Map<NoteDetailsVm>(entity);
+
+    }
+}
